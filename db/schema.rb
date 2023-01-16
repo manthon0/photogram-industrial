b6ed95754ec4b64284fd9c401e07157607e0cd36
manthon0 <manthon0@chicagobooth.edu>
2023-01-16 20:18:10 +0000

    added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_16_194356) do
+ActiveRecord::Schema.define(version: 2023_01_16_201547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2023_01_16_194356) do
   create_table "follow_requests", force: :cascade do |t|
     t.bigint "recipient_id", null: false
     t.bigint "sender_id", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["recipient_id"], name: "index_follow_requests_on_recipient_id"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2023_01_16_194356) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.citext "username"
-    t.boolean "private"
+    t.boolean "private", default: true
     t.integer "likes_count", default: 0
     t.integer "comments_count", default: 0
     t.datetime "created_at", precision: 6, null: false
