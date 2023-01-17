a2cbc87dfee205af3da432bc5cdc43f467ab3f3d
manthon0 <manthon0@chicagobooth.edu>
2023-01-17 03:07:12 +0000

    added and refactored own photos and liked photos

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos
-  get"/:username" => "users#show"
+  get":username/liked" => "photos#liked", as: :liked_photos
+  get"/:username" => "users#show", as: :user
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
